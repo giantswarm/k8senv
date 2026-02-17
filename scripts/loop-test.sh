822d7be5ca7ffa88dc9fc5dbaefc5ac6fd07ab13
Alex Dabija <1087557+alex-dabija@users.noreply.github.com>
2026-02-17 20:46:13 +0000

    build(scripts): add per-iteration elapsed time to loop-test.sh
    Knowing how long each iteration takes helps spot performance regressions
    and estimate time-to-failure when hunting for flaky tests.

--- scripts/loop-test.sh
+++ scripts/loop-test.sh
@@ -17,14 +17,17 @@ tmplog="run-${target}-tmp.log"
 while true; do
     printf '\n\033[0;36m--- Iteration %d: make %s %s ---\033[0m\n' "$iteration" "$target" "$*"
 
+    start=$SECONDS
     if make "$target" "$@" 2>&1 | tee -a "$tmplog"; then
-        printf '\033[0;32mIteration %d passed (%d failures so far)\033[0m\n' "$iteration" "$failures"
+        elapsed=$((SECONDS - start))
+        printf '\033[0;32mIteration %d passed (%d failures so far) (%dm %ds)\033[0m\n' "$iteration" "$failures" $((elapsed/60)) $((elapsed%60))
     else
+        elapsed=$((SECONDS - start))
         failures=$((failures + 1))
         logfile=$(printf 'run-%s-%03d.log' "$target" "$failures")
         mv "$tmplog" "$logfile"
         : > "$tmplog"
-        printf '\033[0;31mIteration %d failed (failure #%d, see %s)\033[0m\n' "$iteration" "$failures" "$logfile"
+        printf '\033[0;31mIteration %d failed (failure #%d, see %s) (%dm %ds)\033[0m\n' "$iteration" "$failures" "$logfile" $((elapsed/60)) $((elapsed%60))
     fi
 
     iteration=$((iteration + 1))
